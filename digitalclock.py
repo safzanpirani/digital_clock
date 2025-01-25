bd689500140f38d5efb31e1146de5110955926dc
Safzan Pirani <safzanpirani@gmail.com>
2025-01-25 22:48:37 +0530

    resizable clock, use relative instead

--- digitalclock.py
+++ digitalclock.py
@@ -38,61 +38,58 @@ def date_time():
 
 
 clock = Tk()
-clock.title(' *** VINITHA CLOCK***')
+clock.title(' *** VINITHA CLOCK ***')
 clock.geometry('1000x500')
 clock.config(bg='black')
+clock.resizable(True, True)
 
 
 lab_hr=Label(clock,text="00",font=('Time New Roman',60,"bold"),bg='red', fg="white")
-lab_hr.place (x=120,y=50, height=110, width=100)
+lab_hr.place(relx=0.12, rely=0.1, relheight=0.22, relwidth=0.1)
 lab_hr_text=Label(clock,text="HOUR",font=('Time New Roman',20,"bold"),bg='red', fg="white")
-lab_hr_text.place (x=120,y=190, height=40, width=100)
+lab_hr_text.place(relx=0.12, rely=0.38, relheight=0.08, relwidth=0.1)
 
 
-lab_min=Label (clock, text="00", font=('Time New Roman', 60, "bold"), bg='red', fg="white")
-lab_min.place (x=340,y=50, height=110, width=100)
+lab_min=Label(clock, text="00", font=('Time New Roman', 60, "bold"), bg='red', fg="white")
+lab_min.place(relx=0.34, rely=0.1, relheight=0.22, relwidth=0.1)
 lab_min_text=Label(clock,text="MIN",font=('Time New Roman',20,"bold"),bg='red', fg="white")
-lab_min_text.place( x =340,y=190,height=40, width=100)
+lab_min_text.place(relx=0.34, rely=0.38, relheight=0.08, relwidth=0.1)
 
 
-lab_sec=Label (clock, text="00", font=('Time New Roman', 60, "bold"), bg='red', fg="white")
-lab_sec.place (x=560,y=50, height=110, width=100)
+lab_sec=Label(clock, text="00", font=('Time New Roman', 60, "bold"), bg='red', fg="white")
+lab_sec.place(relx=0.56, rely=0.1, relheight=0.22, relwidth=0.1)
 lab_sec_text=Label(clock,text="SEC",font=('Time New Roman',20,"bold"),bg='red', fg="white")
-lab_sec_text.place( x =560,y=190,height=40, width=100)
+lab_sec_text.place(relx=0.56, rely=0.38, relheight=0.08, relwidth=0.1)
 
 
-lab_am=Label (clock, text="00", font=('Time New Roman', 50, "bold"), bg='red', fg="white")
-lab_am.place (x=780,y=50, height=110, width=100)
+lab_am=Label(clock, text="00", font=('Time New Roman', 50, "bold"), bg='red', fg="white")
+lab_am.place(relx=0.78, rely=0.1, relheight=0.22, relwidth=0.1)
 lab_am_text=Label(clock,text="AM/PM",font=('Time New Roman',20,"bold"),bg='red', fg="white")
-lab_am_text.place( x =780,y=190,height=40, width=100)
-
+lab_am_text.place(relx=0.78, rely=0.38, relheight=0.08, relwidth=0.1)
 
 
 lab_date=Label(clock,text="00",font=('Time New Roman',60,"bold"),bg='red', fg="white")
-lab_date.place (x=120,y=270, height=110, width=100)
+lab_date.place(relx=0.12, rely=0.54, relheight=0.22, relwidth=0.1)
 lab_date_text=Label(clock,text="DATE",font=('Time New Roman',20,"bold"),bg='red', fg="white")
-lab_date_text.place (x=120,y=410, height=40, width=100)
+lab_date_text.place(relx=0.12, rely=0.82, relheight=0.08, relwidth=0.1)
 
 
-
-lab_mon=Label (clock, text="00", font=('Time New Roman', 60, "bold"), bg='red', fg="white")
-lab_mon.place (x=340,y=270, height=110, width=100)
+lab_mon=Label(clock, text="00", font=('Time New Roman', 60, "bold"), bg='red', fg="white")
+lab_mon.place(relx=0.34, rely=0.54, relheight=0.22, relwidth=0.1)
 lab_mon_text=Label(clock,text="MON",font=('Time New Roman',20,"bold"),bg='red', fg="white")
-lab_mon_text.place( x =340,y=410,height=40, width=100)
+lab_mon_text.place(relx=0.34, rely=0.82, relheight=0.08, relwidth=0.1)
 
 
-
-lab_year=Label (clock, text="00", font=('Time New Roman', 60, "bold"), bg='red', fg="white")
-lab_year.place (x=560,y=270, height=110, width=100)
+lab_year=Label(clock, text="00", font=('Time New Roman', 60, "bold"), bg='red', fg="white")
+lab_year.place(relx=0.56, rely=0.54, relheight=0.22, relwidth=0.1)
 lab_year_text=Label(clock,text="YEAR",font=('Time New Roman',20,"bold"),bg='red', fg="white")
-lab_year_text.place( x =560,y=410,height=40, width=100)
+lab_year_text.place(relx=0.56, rely=0.82, relheight=0.08, relwidth=0.1)
 
 
-
-lab_day=Label (clock, text="00", font=('Time New Roman', 50, "bold"), bg='red', fg="white")
-lab_day.place (x=780,y=270, height=110, width=100)
+lab_day=Label(clock, text="00", font=('Time New Roman', 50, "bold"), bg='red', fg="white")
+lab_day.place(relx=0.78, rely=0.54, relheight=0.22, relwidth=0.1)
 lab_day_text=Label(clock,text="DAY",font=('Time New Roman',20,"bold"),bg='red', fg="white")
-lab_day_text.place( x =780,y=410,height=40, width=100)
+lab_day_text.place(relx=0.78, rely=0.82, relheight=0.08, relwidth=0.1)
 
 
 
